ab775c2714d6406a283d2612823c260578490e98
Jason Washburn <35488541+jasonwashburn@users.noreply.github.com>
2023-03-03 20:08:08 +0000

    adds linting stage to dagger

--- pipeline.py
+++ pipeline.py
@@ -9,12 +9,12 @@ import dagger
 async def test() -> None:
     """Run unit tests in dagger."""
     versions = ["3.10", "3.11"]
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+    async with dagger.Connection(dagger.Config(log_output=sys.stdout)) as client:
         # get reference to the local project
         src = client.host().directory(".")
 
         for version in versions:
-            python = (
+            base = (
                 client.container().from_(f"python:{version}-slim-buster")
                 # mount cloned repository into image
                 .with_mounted_directory("/src", src)
@@ -22,14 +22,22 @@ async def test() -> None:
                 .with_workdir("/src")
                 # install test dependencies
                 .with_exec(["pip", "install", "-r", "requirements.txt"])
-                # run tests
-                .with_exec(["pytest", "."])
             )
             print(f"Starting tests for Python {version}")
+            tests = (
+                # run tests
+                base.with_exec(["pytest", "."])
+            )
 
             # execute
-            await python.exit_code()
+            await tests.exit_code()
             print(f"Tests for Python {version} succeeded!")
+
+            if version == "3.10":
+                print("Starting linting stage...")
+                linter = base.with_exec(["ruff", "."]).with_exec(["mypy", "app/"])
+                await linter.exit_code()
+                print("Linting has finished")
 
     print("All tasks have finished")
 
