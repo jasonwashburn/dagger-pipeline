19dcb6947637974ce00ae1fa21a77eec6adb44b9
Jason Washburn <35488541+jasonwashburn@users.noreply.github.com>
2023-03-03 20:38:46 +0000

    adds caching to dagger pipeline runners

--- pipeline.py
+++ pipeline.py
@@ -11,33 +11,45 @@ async def test() -> None:
     versions = ["3.10", "3.11"]
     async with dagger.Connection(dagger.Config(log_output=sys.stdout)) as client:
         # get reference to the local project
-        src = client.host().directory(".")
+        src = client.host().directory(".", exclude=["./venv"])
 
         for version in versions:
+            venv_cache = client.cache_volume(f"venv-{version}")
             base = (
                 client.container().from_(f"python:{version}-slim-buster")
                 # mount cloned repository into image
                 .with_mounted_directory("/src", src)
+                # mount cached venv
+                .with_mounted_cache("/src/venv", venv_cache)
                 # set current working directory for next commands
-                .with_workdir("/src")
+                # .with_workdir("/src")
+                # create virtual environment
+                .with_exec(["python", "-m", "venv", "/src/venv"])
+                # activate virtual environment
+                .with_env_variable("PATH", "/src/venv/bin")
+                # .with_exec(["source", "/src/venv/bin/activate"])
+                # upgrade pip
+                .with_exec(["pip", "install", "--upgrade", "pip"])
                 # install test dependencies
-                .with_exec(["pip", "install", "-r", "requirements.txt"])
+                .with_exec(["pip", "install", "-r", "/src/requirements.txt"])
             )
+
+            runner = base.with_workdir("/src")
+            if version == "3.10":
+                print("Starting linting stage...")
+                linter = runner.with_exec(["ruff", "."]).with_exec(["mypy", "app/"])
+                await linter.exit_code()
+                print("Linting has finished")
+
             print(f"Starting tests for Python {version}")
             tests = (
                 # run tests
-                base.with_exec(["pytest", "."])
+                runner.with_exec(["pytest", "."])
             )
 
             # execute
             await tests.exit_code()
             print(f"Tests for Python {version} succeeded!")
-
-            if version == "3.10":
-                print("Starting linting stage...")
-                linter = base.with_exec(["ruff", "."]).with_exec(["mypy", "app/"])
-                await linter.exit_code()
-                print("Linting has finished")
 
     print("All tasks have finished")
 
